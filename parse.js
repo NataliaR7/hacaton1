adaab190cac8a0ac17fad6dc657904cff6e3ed73
Natalia <natasha.rogachyova@mail.ru>
2019-12-21 13:41:34 +0500

    add parse form

--- parse.js
+++ parse.js
@@ -1,3 +1,34 @@
-let source = [{type : '1', answer : [], question : '11'}, {type : '11', answer : [], question : '11'}];
+let source = [{type : 'radio', answer : ['a', 'b'], question : '11'}, {type : 'text', answer : [''], question : '222'}, {type : 'checkBox', answer : ['n', 'nn', 'nnn'], question : '333'}];
 
-new Form
+let form = document.querySelector('.mainForm');
+
+for(let i = 0; i < source.length; i++) {
+    let label = document.createElement('label');
+    form.append(label);
+    let question = document.createTextNode(source[i].question);
+    label.append(question);
+    for(let j = 0; j < source[i].answer.length; j++) {
+        let answer = document.createElement('input');
+        switch (source[i].type) {
+            case 'radio' :
+                answer.type = 'radio';
+                answer.name = `answerR${i}`;
+                answer.value = `${source[i].answer[j]}`;
+                break;
+            case 'checkBox' :
+                answer.type = 'checkBox';
+                answer.name = `answerCH${i}`;
+                answer.value = `${source[i].answer[j]}`;
+                break;
+            case 'text' :
+                answer.type = 'text';
+                answer.name = 'textAnswer';
+                break;
+        }
+        label.append(answer);
+        if(source[i].type !== 'text')
+        {
+            label.append(`${source[i].answer[j]}`);
+        }
+    }
+}

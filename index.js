558d99b1062db287a8a52b91e6e5d122dc8d2c08
Olga Bursina <bursina.olya@yandex.ru>
2019-12-21 15:08:57 +0500

    created startForm

---
+++ index.js
@@ -0,0 +1,66 @@
+let questionCount = 0;
+
+function addQuestion() {
+    questionCount++;
+    let form = document.querySelector('#questionForm');
+    let div = document.createElement('div');
+    div.classList.add('questionItem');
+    let question = document.createElement('input');
+    question.setAttribute('type', 'text');
+    question.placeholder = 'Введите вопрос';
+    div.insertAdjacentElement('beforeend', question);
+    let type = document.createElement('input');
+    type.setAttribute('type', 'radio');
+    let typeArray = [getRadioElement('radio'), getRadioElement('checkbox'), getRadioElement('text')];
+    for (let element of typeArray) {
+        element.addEventListener('change', () => {
+            if (element.getAttribute('needAnswers') === 'true') {
+                let answerContainer = document.createElement('div');
+                answerContainer.setAttribute('id', 'answerContainer');
+                answerContainer.textContent = 'Ответы:';
+                let answerCount = 0;
+                let button = document.createElement('button');
+                button.textContent = 'Добавить ответ';
+                button.setAttribute('type', 'button');
+                button.addEventListener('click', () => {
+                    answerCount++;
+                    let questionDiv = document.createElement('div');
+                    let span = document.createElement('span');
+                    span.textContent = answerCount.toString();
+                    let input = document.createElement('input');
+                    input.setAttribute('type', 'text');
+                    questionDiv.insertAdjacentElement('beforeend', span);
+                    questionDiv.insertAdjacentElement('beforeend', input);
+                    answerContainer.insertAdjacentElement('beforeend', questionDiv);
+                    div.insertAdjacentElement('beforeend', answerContainer);
+                });
+                answerContainer.insertAdjacentElement('beforeend', button);
+                if (!div.querySelector('#answerContainer')) {
+                    div.insertAdjacentElement('beforeend', answerContainer);
+                }
+            }
+            else {
+                div.querySelector('#answerContainer').remove();
+            }
+        });
+        div.insertAdjacentElement('beforeend', element);
+    }
+    form.insertAdjacentElement('beforeend', div);
+    document.body.insertAdjacentElement('beforeend', form)
+}
+
+function getRadioElement(text) {
+    let label = document.createElement('label');
+    label.textContent = text;
+    let radioInput = document.createElement('input');
+    radioInput.setAttribute('type', 'radio');
+    radioInput.setAttribute('name', 'questionType' + questionCount);
+    if (text === 'radio' || text === 'checkbox') {
+        label.setAttribute('needAnswers', 'true')
+    } else {
+        label.setAttribute('needAnswers', 'false')
+    }
+    label.insertAdjacentElement('afterbegin', radioInput)
+    return label;
+}
+
